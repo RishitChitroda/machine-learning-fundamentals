87866b3406870e90f9da67b8a6c44bc2eb72b4c4
RishitChitroda <rishitchitroda@gmail.com>
2026-06-07 12:52:56 +0530

    Add classification examples using Iris dataset

---
+++ Algorithms/Classifier/iris_random_forest_classifier.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+iris = load_iris()
+
+x = iris.data
+y = iris.target
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier()
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+accuracy
